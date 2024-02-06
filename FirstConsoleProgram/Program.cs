2b37d15660e3a218bf0e2ddeebdd1a23de81146e
admin <admin@DESKTOP-3F5N14B>
2024-02-06 17:26:14 +0530

    Add project files.

---
+++ FirstConsoleProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+/*
+ * This is the multi line comment 
+ * This is the Frist C# program
+ */
+
+namespace CShartpTutorials
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("I am learning c#");
+            Console.WriteLine(3 + 3);
+            Console.Write("Hello World");
+            Console.Write("I print on the same line"); //This is the single line comment
+
+        }
+    }
+}
