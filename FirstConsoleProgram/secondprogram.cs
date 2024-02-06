29878d88e27365519fb1a1d068f2d1af0f0bdacb
gsudarshan1990 <sudarshan2009@live.in>
2024-02-06 18:19:05 +0530

    Secondprogram

---
+++ FirstConsoleProgram/secondprogram.cs
@@ -0,0 +1,23 @@
+﻿namespace SecondProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "John";
+            int myNum = 15;
+            char myLetter = 'a';
+            bool myBool = false;
+            double myDouble = 5.5D;
+            float myFloat = 1.0f;
+
+            Console.WriteLine(name);
+            Console.WriteLine(myNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myFloat);
+
+        }
+    }
+}
