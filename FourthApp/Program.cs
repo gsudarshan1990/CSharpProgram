e3d7f04272bb1d3295b786683f579190fda7db77
gsudarshan1990 <sudarshan2009@live.in>
2024-02-07 12:50:27 +0530

    Commit 1

---
+++ FourthApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace FunctionExample
+{
+    class Function
+    {
+        static void Method()
+        {
+            Console.WriteLine("This is executing from the Method");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("This is executing from the Method2");
+        }
+
+        static void Method3()
+        {
+            Console.WriteLine("This is executing from the Method3");
+        }
+
+        static void Main(String[] args)
+        {
+
+            Method();
+            Method2();
+            Method3();
+        }
+      
+    }
+}
